dec19fc0375b1f9bda895ac19aeb685de2acbad2
Graham Home <grahamhome333@gmail.com>
2017-02-11 17:37:27 -0500

    Added basic needle control
    Added function to step needle through different positions & interrupt
    with button press.

--- graham's test code/experimental_stepper_code/experimental_stepper_code.ino
+++ graham's test code/experimental_stepper_code/experimental_stepper_code.ino
@@ -1,36 +1,103 @@
-/*     Simple Stepper Motor Control Exaple Code
- *      
- *  by Dejan Nedelkovski, www.HowToMechatronics.com
- *  
+/*  Use a stepper motor as a dial gauge.
+ *  Author: Graham Home
  */
-// defines pins numbers
-const int stepPin = 3; 
-const int dirPin = 4; 
+
+// Define pin numbers
+const int stepPin = 9; // Set high to start steppin'
+const int dirPin = 8; // Sets direction
+const int btnPin = 3; // Input pin for pushbutton
+
+// Max/min values for needle distance
+const int minPos = 0;
+const int maxPos = 600;
+
+// Variable to determine how far the needle will move
+int increment = 20;
+
+// Variable to determine the direction in which the needle will move
+bool clockwise = true;
+
+// On/off toggle
+bool on = true;
+
+/**
+ * Resets the needle to the starting position.
+ */
+void resetNeedle() {
+  digitalWrite( dirPin, LOW );
+  for( int x = 0; x < maxPos; x++ ) {
+    digitalWrite( stepPin, HIGH ); 
+    delayMicroseconds( 500 ); 
+    digitalWrite( stepPin, LOW ); 
+    delayMicroseconds( 500 ); 
+  }
+}
+
+/**
+ * Moves the needle the specified distance.
+ * @param dist : The distance to move.
+ */
+void move(int dist) {
+  for( int x = 0; x < dist; x++ ) {
+    digitalWrite( stepPin, HIGH ); 
+    delayMicroseconds( 500 ); 
+    digitalWrite( stepPin, LOW ); 
+    delayMicroseconds( 500 ); 
+  }
+}
+
+/**
+ * Steps the needle all the way forward, then back.
+ */
+void stepFwdAndBack() {
+  if ( clockwise ) {
+    digitalWrite( dirPin, HIGH );
+  } else {
+    digitalWrite( dirPin, LOW );
+  }
+  int pos = minPos;
+  while ( pos < maxPos ) {
+    if ( on ) {
+      move(increment);
+      pos += increment;
+    }
+    delay(250);
+  }
+  clockwise = !clockwise;
+  delay(1000);
+}
+
+/**
+ * Checks the pushbutton switch.
+ */
+void checkButton() {
+  if ( digitalRead( btnPin ) ) {
+    delay ( 500 );
+    if ( digitalRead ( btnPin ) ) {
+      on = !on;
+      delay( 1000 );
+    }
+  }
+}
+
+/**
+ * Toggles the program's 'on' switch.
+ */
+void toggle() {
+  on = !on;
+}
  
 void setup() {
-  // Sets the two pins as Outputs
-  pinMode(stepPin,OUTPUT); 
-  pinMode(dirPin,OUTPUT);
+  // Set up I/O pins
+  pinMode( stepPin, OUTPUT ); 
+  pinMode( dirPin, OUTPUT );
+  pinMode( btnPin, INPUT );
+  attachInterrupt( digitalPinToInterrupt(btnPin), toggle, FALLING );
+  resetNeedle();
+  
+  
 }
 void loop() {
-  int stepTime = 1000;
-  digitalWrite(dirPin,HIGH); // Enables the motor to move in a particular direction
-  // Makes 200 pulses for making one full cycle rotation
-  for(int x = 0; x < 400; x++) {
-    digitalWrite(stepPin,HIGH); 
-    delayMicroseconds(stepTime); 
-    digitalWrite(stepPin,LOW); 
-    delayMicroseconds(stepTime); 
-  }
-  delay(2000); // One second delay
-  
-  digitalWrite(dirPin,LOW); //Changes the rotations direction
-  // Makes 400 pulses for making two full cycle rotation
-  for(int x = 0; x < 800; x++) {
-    digitalWrite(stepPin,HIGH);
-    delayMicroseconds(stepTime);
-    digitalWrite(stepPin,LOW);
-    delayMicroseconds(stepTime);
-  }
-  delay(2000);
+  //checkButton();
+  stepFwdAndBack();
 }
